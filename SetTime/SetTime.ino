5f2142e0229c040143bf8eeca0f91cc3f2ad76ed
Mathijs <matezer@gmail.com>
2017-11-29 02:22:50 +0100

    It works

--- SetTime/SetTime.ino
+++ SetTime/SetTime.ino
@@ -1,56 +1,48 @@
 
+#include <math.h>
 #include <Wire.h>
 #include "PCF85063TP.h"
 
 PCD85063TP clock;//define a object of PCD85063TP class
 
-void setup() {
-  Serial.begin(9600);
-  clock.begin();
-  byte year[4];
-  byte month[2];
-  byte day[2];
-  byte hour[2];
-  byte minute[2];
-  byte sec[2];
-
-getDat(year);
-getDat(month);
-getDat(day);
-getDat(hour);
-getDat(minute);
-getDat(sec);
+  int alarmHour = 7;
+  int alarmMinute = 30;
 
 
-  clock.stopClock();
-  clock.fillByYMD(year,data[1],data[2]);//Jan 19,2013
-  clock.fillByHMS(data[3],data[4],data[5]);//15:28 30"
-  clock.fillDayOfWeek(TUE);//Saturday
-  clock.setTime();//write time to the RTC chip
-  clock.startClock();
-
-  //clock.setcalibration(1, 32767.2);  // Setting offset by clock frequency
-  uint8_t ret = clock.calibratBySeconds(0, -0.000041);
-  Serial.print("offset value: ");
-  Serial.print("0x");
-  Serial.println(ret, HEX);
- 
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-    printTime();
-  delay(1000);
-}
-
-byte getDat(*byte[] timeUnit){
-  for int i = 0; i < sizeof(timeUnit); i++){
+char getDat(char timeUnit[], int arrayLength) {
+  for (int i = 0; i < arrayLength; i++) {
     while (!Serial.available()) {
       delay(10);
     }
     timeUnit[i] = Serial.read();
   }
-  
+
+}
+
+void beep(int seconds) {
+  for(int i = 0; i < seconds; i++) {
+  digitalWrite(7, HIGH);
+  delay(500);                  
+  digitalWrite(7, LOW);        
+  delay(500); 
+  }
+  return;
+}
+
+int changeTypeToInt(char timeUnit[], int arrayLength) {
+
+  double temp = 0.0;
+  int j = 0;
+
+  Serial.print("length: ");
+  Serial.println(arrayLength);
+
+  for (int i = arrayLength - 1; i >= 0; i--) {
+    int parsed = (int) timeUnit[i];
+    temp = pow(10,j) * (parsed - 48) + temp;
+    j++;
+  }
+  return round(temp);
 }
 
 void printTime()
@@ -66,7 +58,7 @@ void printTime()
   Serial.print("/");
   Serial.print(clock.dayOfMonth, DEC);
   Serial.print("/");
-  Serial.print(clock.year+2000, DEC);
+  Serial.print(clock.year + 2000, DEC);
   Serial.print(" ");
   Serial.print(clock.dayOfMonth);
   Serial.print("*");
@@ -96,3 +88,50 @@ void printTime()
   }
   Serial.println(" ");
 }
+
+void setup() {
+
+  pinMode(7, OUTPUT); 
+  
+  Serial.begin(9600);
+  clock.begin();
+  char year[4];
+  char month[2];
+  char day[2];
+  char hour[2];
+  char minute[2];
+  char sec[2];
+
+  getDat(year, 4);
+  getDat(month, 2);
+  getDat(day, 2);
+  getDat(hour, 2);
+  getDat(minute, 2);
+  getDat(sec, 2);
+
+
+  clock.stopClock();
+  clock.fillByYMD(changeTypeToInt(year, 4), changeTypeToInt(month, 2), changeTypeToInt(day, 2)); //Jan 19,2013
+  clock.fillByHMS(changeTypeToInt(hour, 2), changeTypeToInt(minute, 2), changeTypeToInt(sec, 2)); //15:28 30"
+  clock.fillDayOfWeek(TUE);//Saturday
+  clock.setTime();//write time to the RTC chip
+  clock.startClock();
+
+  //clock.setcalibration(1, 32767.2);  // Setting offset by clock frequency
+  uint8_t ret = clock.calibratBySeconds(0, -0.000041);
+  Serial.print("offset value: ");
+  Serial.print("0x");
+  Serial.println(ret, HEX);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  printTime();
+  if(clock.hour == alarmHour && clock.minute == alarmMinute){
+    beep(3);
+  }
+  delay(1000);
+}
+
+
